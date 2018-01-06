f2040c2d8f722597402e32d58bc7e6094909d153
adeeb2358 <adeeb2358@gmail.com>
2018-01-06 12:57:22 +0530

    new updates

---
+++ bigHeader.h
@@ -0,0 +1,13 @@
+#ifndef BIG_HEADER_H
+#define BIG_HEADER_H
+
+#include <iostream>
+#include <stdio.h>
+#include <sys/sem.h>
+#include <ctime>
+#include <cstdlib>
+#include <unistd.h>
+
+
+
+#endif // BIG_HEADER_H
